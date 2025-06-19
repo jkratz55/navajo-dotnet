78f2cf62d6a1fe2351c957fa6374cbb8c9063a29
Joseph Kratz <4985721+jkratz55@users.noreply.github.com>
2025-06-19 14:53:44 -0400

    Added OpenTelemetry traces and metrics

--- navajo-dotnet/Program.cs
+++ navajo-dotnet/Program.cs
@@ -1,6 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using navajo_dotnet.Data;
 using navajo_dotnet.Service;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +20,42 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddSingleton<IEncryptionService, AesEncryptionService>();
 
+// Add OpenTelemetry
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource =>
+        resource.AddService(serviceName: "navajo", serviceVersion: "0.1.0"))
+    .WithMetrics(metrics =>
+        metrics
+            .AddAspNetCoreInstrumentation()
+            .AddRuntimeInstrumentation()
+            .AddMeter("NavajoMeter")
+            .AddPrometheusExporter())
+    .WithTracing(tracing =>
+    {
+        tracing
+            .SetSampler(new AlwaysOnSampler())
+            .AddAspNetCoreInstrumentation(opts =>
+            {
+                opts.RecordException = true;
+                opts.EnrichWithException = (activity, exception) =>
+                {
+                    activity.SetTag("exception.message", exception.Message);
+                    activity.SetTag("exception.stacktrace", exception.StackTrace);
+                };
+            })
+            .AddHttpClientInstrumentation()
+            .AddEntityFrameworkCoreInstrumentation(opts =>
+            {
+                opts.SetDbStatementForText = true;
+                opts.SetDbStatementForStoredProcedure = true;
+            })
+            .AddOtlpExporter(opts =>
+            {
+                opts.Protocol = OtlpExportProtocol.HttpProtobuf;
+                opts.Endpoint = new Uri("http://localhost:4318/v1/traces"); // Default OTLP endpoint for Jaeger
+            });
+    });
+
 
 var app = builder.Build();
 
@@ -24,10 +65,9 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
